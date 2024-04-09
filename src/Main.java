8f5539c800235ac34508858b5a37305fbe98140c
Dmitry Rassadnikov <dmitryrassadnikov@gmail.com>
2024-04-09 16:05:05 +0300

    Внесены корректироваки по счетчику и доработан выход из программы.

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,23 @@ public class Main {
     public static void main(String[] args) {
         int i = 0;
         while (true) {
-            i++;
             System.out.println("Укажите путь к файлу:");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
-            if ((fileExists == false) || (isDirectory == true)) {
+            String exit = "выход";
+            if (path.compareTo(exit) == 0) {
+                break;
+            } else if ((fileExists == false) || (isDirectory == true)) {
                 System.out.println("Файл не существует или указанный путь является путём к папке, а не к файлу!");
                 continue;
             } else {
                 System.out.println("Путь указан верно!");
-                System.out.println("Это файл номер: "+i);
-                break;
+                i++;
             }
+            System.out.println("Это файл номер: " + i);
         }
     }
 }
+
