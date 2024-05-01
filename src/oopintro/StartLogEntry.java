e4e31047e005c6931e61a04574435ea7826fb3d3
Dmitry Rassadnikov <dmitryrassadnikov@gmail.com>
2024-05-01 21:12:35 +0300

    Курсовой проект по теме Collections. Задание #2.

--- src/oopintro/StartLogEntry.java
+++ src/oopintro/StartLogEntry.java
@@ -19,9 +19,9 @@ public class StartLogEntry {
         List<LocalDateTime> dates = new ArrayList<>();
         for (String line : lines) {
             logEntry = new LogEntry(line);
-          //  System.out.println("Строка из лог-файла: " + logEntry);
+            //  System.out.println("Строка из лог-файла: " + logEntry);
             UserAgent userAgent = new UserAgent(logEntry.getUserAgent());
-            System.out.print("Операционная система: " + userAgent.getOperatingSystem() + "\n" + "Браузер:" + userAgent.getBrowser() + "\n");
+            // System.out.print("Операционная система: " + userAgent.getOperatingSystem() + "\n" + "Браузер:" + userAgent.getBrowser() + "\n");
             statistics.addEntry(logEntry);
             dates.add(logEntry.getDateTime());
         }
@@ -33,8 +33,10 @@ public class StartLogEntry {
         System.out.printf("Минимальная дата: %s\n", minTime);
         System.out.printf("Максимальная дата: %s\n", maxTime);
         System.out.println("Период времени в часах = " + hoursDiff);
-        System.out.printf("Средний объем трафика за час: " + "%.1f", statistics.getTrafficRate() / hoursDiff);
-        System.out.println("Страницы: " + statistics.getPages());
-        System.out.println("Статистика ОС: " + statistics.getOperatingSystemsStatistics());
+        System.out.printf("Средний объем трафика за час: " + "%.1f\n", statistics.getTrafficRate() / hoursDiff);
+        System.out.println("Статистика операционных систем пользователей сайта: " + statistics.getOperatingSystemsStatistics());
+        System.out.println("Статистика браузеров пользователей сайта: " + statistics.getBrowserStatistics());
+        System.out.println("Список всех существующих страниц сайта: " + statistics.getPages());
+        System.out.println("Список всех несуществующих страниц сайта: " + statistics.getNonExistingPages());
     }
 }
--- src/oopintro/Statistics.java
+++ src/oopintro/Statistics.java
@@ -6,9 +6,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-
-
-
 public class Statistics {
     private int totalTraffic;
     private LocalDateTime minTime;
@@ -16,6 +13,8 @@ public class Statistics {
     private String date;
     private Map<String, Integer> operatingSystems;
     private Set<String> pages;
+    private HashSet<String> nonExistingPages = new HashSet<>();
+    private HashMap<String, Integer> browserStats = new HashMap<>();
 
     public Statistics() {
         operatingSystems = new HashMap<>();
@@ -32,19 +31,25 @@ public class Statistics {
             operatingSystems.put(UserAgent.getOperatingSystem() + "/" + UserAgent.getBrowser(), count + 1);
         }
         if (logEntry.getResponseCode() == 200) {
-            //pages.add(path);
             pages.add(logEntry.getPath());
         }
-
+        if (logEntry.getResponseCode() == 404) {
+            nonExistingPages.add(logEntry.getPath());
+        }
+        if (!browserStats.containsKey(UserAgent.getBrowser())) {
+            browserStats.put(UserAgent.getBrowser(), 1);
+        } else {
+            browserStats.put(UserAgent.getBrowser(), browserStats.get(UserAgent.getBrowser()) + 1);
+        }
     }
 
     public double getTrafficRate() {
         System.out.println("Общий трафик в байтах: " + totalTraffic);
         return totalTraffic;
     }
+
     public Map<String, Double> getOperatingSystemsStatistics() {
         int total = operatingSystems.values().stream().mapToInt(Integer::intValue).sum();
-        // System.out.println("ТЕСТ" + total);
         Map<String, Double> result = new HashMap<>();
         for (Map.Entry<String, Integer> entry : operatingSystems.entrySet()) {
             result.put(entry.getKey(), (double) entry.getValue() / total);
@@ -54,5 +59,20 @@ public class Statistics {
 
     public Set<String> getPages() {
         return pages;
+    }
+
+    public Map<String, Double> getBrowserStatistics() {
+        int totalBrowsers = browserStats.size();
+        Map<String, Double> result = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : browserStats.entrySet()) {
+            String browser = entry.getKey();
+            int count = entry.getValue();
+            result.put(browser, (double) count / totalBrowsers);
+        }
+        return result;
+    }
+
+    public HashSet<String> getNonExistingPages() {
+        return nonExistingPages;
     }
 }
